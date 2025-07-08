78223e0a1021af2a5122716c0bb7530ccfd5bd9c
podo <photoppodo@naver.com>
2025-07-08 14:52:17 +0900

    25/06/27 2023 kakao blind recruitment : 개인정보 수집 유효기간 코틀린 풀이

---
+++ src/2025/06/27/Main.kt
@@ -0,0 +1,52 @@
+package `2025`.`06`.`27`
+
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+fun main() {
+    val today = "2022.05.19"
+    val formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd")
+    val todayDate = LocalDate.parse(today, formatter)
+
+    val terms = listOf("A 6", "B 12", "C 3")
+    val privacies = listOf("2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C")
+
+    val privacyMatrix = Array<Triple<String, LocalDate, Int>?>(privacies.size) { null }
+
+    for (i in privacies.indices) {
+        val privacy = privacies[i].split(" ")
+
+        val date = privacy.first() // 2021.05.02
+        val type = privacy.last() // A
+
+        val localDate = LocalDate.parse(date, formatter)
+        privacyMatrix[i] = Triple(type, localDate, 0)
+    }
+
+    for (i in terms.indices) {
+        // ["A 6", "B 12"]
+        val term = terms[i].split(" ")
+        val type = term.first() // A
+        val month = term.last().toInt() // 6
+
+        for (j in privacyMatrix.indices) {
+            val old = privacyMatrix[j]!!
+
+            if (privacyMatrix[j]!!.first == type) {
+                privacyMatrix[j] = Triple(old.first, old.second, month)
+            }
+        }
+    }
+
+    for (i in privacyMatrix.indices) {
+        val month = privacyMatrix[i]!!.third.toLong()
+        val days = month * 28.toLong()
+//        val endDate = privacyMatrix[i]!!.second.plusDays(days).minusDays(1)
+
+        val endDate = privacyMatrix[i]!!.second.plusMonths(month).minusDays(1)
+
+        if (endDate.isBefore(todayDate)) {
+            println(i + 1)
+        }
+    }
+}
---
+++ src/2025/06/27/Refactored.kt
@@ -0,0 +1,41 @@
+package `2025`.`06`.`27`
+
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+fun solution(today: String, terms: Array<String>, privacies: Array<String>): IntArray {
+    val formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd")
+    val todayDate = LocalDate.parse(today, formatter)
+
+    val termMap = terms.associate {
+        val (type, months) = it.split(" ")
+        type to months.toInt()
+    }
+
+    val result = mutableListOf<Int>()
+
+    for (i in privacies.indices) {
+        val (date, type) = privacies[i].split(" ")
+        val collectedDate = LocalDate.parse(date, formatter)
+        val months = termMap[type]!!
+
+        val endDate = collectedDate.plusMonths(months.toLong()).minusDays(1)
+
+        if (todayDate.isAfter(endDate)) {
+            result.add(i + 1)
+//            println(i + 1)
+        }
+    }
+
+    return result.toIntArray()
+}
+
+fun main() {
+    val today = "2022.05.19"
+    val formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd")
+
+    val terms = arrayOf("A 6", "B 12", "C 3")
+    val privacies = arrayOf("2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C")
+
+    solution(today, terms, privacies)
+}
